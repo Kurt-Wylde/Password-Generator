61c7e98ba895e772b453a71c31f7b2364f67dac8
Evgeny Koshkin <wylde.kurt@gmail.com>
2021-11-15 17:55:30 +0300

    Initial Commit

---
+++ Password Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password Generator
+//
+//  Created by Evgeny Koshkin on 15.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
